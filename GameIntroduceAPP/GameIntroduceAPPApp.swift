2ec2384fa110f8533785caa57801f584035def33
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2020-10-31 13:04:57 +0900

    Initial Commit

---
+++ GameIntroduceAPP/GameIntroduceAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameIntroduceAPPApp.swift
+//  GameIntroduceAPP
+//
+//  Created by user on 2020/10/31.
+//
+
+import SwiftUI
+
+@main
+struct GameIntroduceAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
